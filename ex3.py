7b8987f625aed6831d6aa251cc5c5d1afe61921b
Hioaney <81027592+Hioaney@users.noreply.github.com>
2021-04-13 23:20:44 +0600

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,33 @@
+# Напишите 3 функции
+
+#3.1
+my_list = ['hello', 5, 'John']
+def func(list):
+    return list
+print(list(['Element', 'start', my_list, 'finish']))
+
+#3.2
+def func(lst):
+    return lst
+print(func('{''x:1, 5:2, John:3''}'))
+
+
+#3.3
+a = (input('Enter only odd numbers:'))
+b = [input('Enter only even numbers:')]
+c = []
+d = []
+lambda a, b: a ** 2, b ** 2
+print(c, d(a, b))
+
+
+
+
+    
+
+
+
+
+
+
+
